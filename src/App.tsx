1b203abd659890665a05ed55276fc4d3ee6bfe18
peterrobert <pwambui93@gmail.com>
2026-01-22 16:41:47 +0300

    Created hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./pages/landing-page";
@@ -6,12 +6,12 @@ import WorkFlowPage from "./pages/work-flow-page";
 // -- Router setup for LandingPage and WorkFlowPage --
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/workflow" element={<WorkFlowPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
