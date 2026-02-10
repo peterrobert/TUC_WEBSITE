9cf1cf1d1e086934f858837be8dc6100f758f782
peterrobert <pwambui93@gmail.com>
2026-02-10 16:37:06 +0300

    Analytics visist added

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import Navigation from "./layouts/Navigation.tsx";
-import { Analytics } from "@vercel/analytics/next";
-
+import { Analytics } from "@vercel/analytics/react";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Navigation />
