ae1d6be47c8670ef8f6ec81808782c828367ad03
peterrobert <pwambui93@gmail.com>
2026-02-10 16:35:03 +0300

    Add a anlytics page

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import Navigation from "./layouts/Navigation.tsx";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Navigation />
     <App />
-  </StrictMode>
+    <Analytics />
+  </StrictMode>,
 );
